0225b4abf3471174abf2b5c49d43ae9ba78a6a7f
hirnukuono <hirnukuono@gmail.com>
2025-04-29 01:21:45 +0300

    dino provides. thanks.

--- NoInterruptions/DinoTermPatch.cs
+++ NoInterruptions/DinoTermPatch.cs
@@ -2,7 +2,6 @@
 using HarmonyLib;
 using LevelGeneration;
 using Player;
-using UnityEngine;
 using System.Collections;
 
 namespace NoInterruptions
@@ -10,58 +9,23 @@ namespace NoInterruptions
     [HarmonyPatch(typeof(LG_ComputerTerminal))]
     internal static class TerminalPatch
     {
-        [HarmonyPatch(nameof(LG_ComputerTerminal.Update))]
-        [HarmonyPrefix]
-        private static void Postfix_CheckPlayerVicinity(LG_ComputerTerminal __instance)
-        {
-            if (__instance.name == "fdrunning") return;
-            if (__instance.CurrentStateName == TERM_State.Sleeping) return;
-            var plrs = PlayerManager.PlayerAgentsInLevel;
-            bool flag = false;
-            foreach (var plr in plrs) if (plr.Alive && UnityEngine.Vector3.Distance(plr.Position, __instance.gameObject.transform.position) < 2f) flag = true;
-            if (!flag)
-            {
-                Debug.Log("NoInterruptions - terminal not sleeping, all players more than 2 meters away..");
-                __instance.name = "fdrunning";
-                CoroutineManager.StartCoroutine(FiveSecondDelay(__instance).WrapToIl2Cpp());
-            }
-        }
-
-
-        [HarmonyPatch(nameof(LG_ComputerTerminal.EnterFPSView))]
-        [HarmonyPostfix]
-        private static void Postfix_EnterInteractingFPS(LG_ComputerTerminal __instance)
-        {
-            if (__instance.CurrentStateName == TERM_State.PlayerInteracting)
-                CoroutineManager.StartCoroutine(DelayedFixState(__instance, __instance.m_localInteractionSource).WrapToIl2Cpp());
-        }
-
         [HarmonyPatch(nameof(LG_ComputerTerminal.SyncChangeState))]
         [HarmonyPostfix]
         private static void Postfix_EnterInteracting(LG_ComputerTerminal __instance)
         {
             if (__instance.CurrentStateName == TERM_State.PlayerInteracting)
-                CoroutineManager.StartCoroutine(DelayedFixState(__instance, __instance.m_syncedInteractionSource).WrapToIl2Cpp());
-        }
-        private static IEnumerator FiveSecondDelay(LG_ComputerTerminal terminal)
-        {
-            yield return new WaitForSeconds(5);
-            var plrs = PlayerManager.PlayerAgentsInLevel;
-            bool flag = false;
-            foreach (var plr in plrs) if (plr.Alive && UnityEngine.Vector3.Distance(plr.Position, terminal.gameObject.transform.position) < 2f) flag = true;
-            if (!flag)
             {
-                Debug.Log("NoInterruptions - terminal not sleeping, players still far away. go to sleeeeep..");
-                terminal.ChangeState(TERM_State.Sleeping);
-                terminal.name = "fdnotrunning";
+                CoroutineManager.StartCoroutine(DelayedFixState(__instance).WrapToIl2Cpp());
             }
-            yield break;
         }
 
-        private static IEnumerator DelayedFixState(LG_ComputerTerminal terminal, PlayerAgent player)
+        private static IEnumerator DelayedFixState(LG_ComputerTerminal terminal)
         {
-            // JFS - Delay checking locomotion in case packet was delayed
             float endTime = Clock.Time + 0.5f;
+            // Wait one frame so we don't interrupt the ChangeState call
+            yield return null;
+
+            // JFS - Delay checking locomotion in case packet was delayed
             while (Clock.Time < endTime)
             {
                 if (terminal.CurrentStateName != TERM_State.PlayerInteracting)
@@ -71,13 +35,21 @@ namespace NoInterruptions
 
             while (terminal.CurrentStateName == TERM_State.PlayerInteracting)
             {
-                AttemptFixState(terminal, player);
+                AttemptFixState(terminal);
                 yield return null;
             }
         }
 
-        private static void AttemptFixState(LG_ComputerTerminal terminal, PlayerAgent player)
+        private static void AttemptFixState(LG_ComputerTerminal terminal)
         {
+            var player = terminal.m_localInteractionSource != null ? terminal.m_localInteractionSource : terminal.m_syncedInteractionSource;
+
+            if (player == null)
+            {
+                terminal.ChangeState(TERM_State.Awake);
+                return;
+            }
+
             if (player.Locomotion.m_currentStateEnum != PlayerLocomotion.PLOC_State.OnTerminal)
             {
                 if (player.IsLocallyOwned || (player.transform.position - player.Sync.m_locomotionData.Pos).sqrMagnitude > 0.0001f)
--- NoInterruptions/Main.cs
+++ NoInterruptions/Main.cs
@@ -2,15 +2,18 @@
 using BepInEx.Unity.IL2CPP;
 using UnityEngine;
 using HarmonyLib;
+using BepInEx.Logging;
 
 namespace NoInterruptions;
 
-[BepInPlugin("NoInterruptions", "NoInterruptions", "0.1.3")]
+[BepInPlugin("NoInterruptions", "NoInterruptions", "0.1.4")]
 
 public class Plugin : BasePlugin
 {
+    internal static ManualLogSource L;
     public override void Load()
     {
+        L = Log;
         Harmony gargle = new("gargle");
         gargle.PatchAll();
         Debug.Log("NoInterruptions - zombie hand sticking out of a dead game's grave");
